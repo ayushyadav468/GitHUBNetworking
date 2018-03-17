9dfe5cb8007311da38a3efe1792c6e7e05195184
ayushyadav468 <ayushyadav468@gmail.com>
2018-03-17 22:32:05 +0530

    initial commit

--- app/src/main/java/com/example/ayushyadav/githubnetworking/MainActivity.java
+++ app/src/main/java/com/example/ayushyadav/githubnetworking/MainActivity.java
@@ -46,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                fetchRepos("ayushyadav468");
+                fetchRepos("rohanarora");
             }
         });
     }
